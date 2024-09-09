cf95a89311ed8e02223f6dd77a1a69764daec501
Reuben Siwela <reubensiwela@gmail.com>
2024-09-09 18:56:03 +0200

    Initial commit

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask, request, jsonify
+import os
+from flask import Flask, request, jsonify # type: ignore
 import numpy as np
 import pickle
 
 app = Flask(__name__)
 
-# Load the Tuned Random Forest Model
-with open('tuned_random_forest_model.pkl', 'rb') as file:
-    model = pickle.load(file)
+# Get the absolute path to the model file
+model_file_path = os.path.abspath('tuned_random_forest_model.pkl')
+print(f"Model file path: {model_file_path}")
+
+# Check if the file exists before loading
+if not os.path.exists(model_file_path):
+    print("Model file not found!")
+else:
+    # Load the Tuned Random Forest Model
+    with open(model_file_path, 'rb') as file:
+        model = pickle.load(file)
 
 @app.route('/')
 def home():
@@ -14,14 +23,11 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get the data from the POST request
     data = request.get_json(force=True)
-    
-    # Extract features in the same order as they were trained
     try:
         features = np.array([
             data['credit_score'],
-            data['gender'],              # Ensure it's aligned with the training columns
+            data['gender'],
             data['age'],
             data['tenure'],
             data['balance'],
@@ -29,19 +35,17 @@ def predict():
             data['has_cr_card'],
             data['is_active_member'],
             data['estimated_salary'],
-            data['geography_Germany'],   # One-hot encoded Geography for Germany
-            data['geography_Spain']      # One-hot encoded Geography for Spain
+            data['geography_Germany'],
+            data['geography_Spain']
         ]).reshape(1, -1)
     except KeyError as e:
         return jsonify({"error": f"Missing feature in input data: {str(e)}"}), 400
     
-    # Make a prediction using the loaded model
     prediction = model.predict(features)
     prediction_proba = model.predict_proba(features)[:, 1]
     
-    # Return the result as a JSON response
     return jsonify({
-        'prediction': int(prediction[0]),  # 0 or 1
+        'prediction': int(prediction[0]),
         'churn_probability': float(prediction_proba[0])
     })
 
